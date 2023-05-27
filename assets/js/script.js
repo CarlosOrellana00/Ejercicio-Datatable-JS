53a19207a46f41cdb9af633d43b3e4603b5175d5
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-27 19:26:32 -0400

    6

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,23 @@ let dataTable;
 let dataTableIsInitialized = false;
 
 const dataTableOptions = {
-  pageLength: 3
+  pageLength: 3,
+  destroy: true,
+  language: {
+      lengthMenu: "Mostrar _MENU_ registros por página",
+      zeroRecords: "Ningún usuario encontrado",
+      info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+      infoEmpty: "Ningún usuario encontrado",
+      infoFiltered: "(filtrados desde _MAX_ registros totales)",
+      search: "Buscar:",
+      loadingRecords: "Cargando...",
+      paginate: {
+          first: "Primero",
+          last: "Último",
+          next: "Siguiente",
+          previous: "Anterior"
+      }
+  }
 };
 
 const initDataTable = async () =>{
