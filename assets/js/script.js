8413dbfb782e21d6585a798ebde3643f208b6086
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-27 20:15:25 -0400

    11

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const dataTableOptions = {
     // { width: "50%", targets:[0]},
     { searchable: false, targets:[1]}
   ],
-  pageLength: 3,
+  pageLength: 3, //cantidad de registros visibles por vista
   destroy: true,
   language: {
       lengthMenu: "Mostrar _MENU_ registros por página",
@@ -46,6 +46,25 @@ const listUsers = async () => {
     // console.log(data);
     let content = ``;
     users.forEach((user, index) => {
+      //Para tener mas de 10 registros
+      // for(let x=0; x<5;x++)
+      // content +=`
+      // <tr>
+      //   <td>${index + 1}</td>
+      //   <td>${user.name}</td>
+      //   <td>${user.email}</td>
+      //   <td>${user.address.city}</td>
+      //   <td>${user.company.name}</td>
+      //   <td><i class="fa-solid fa-check" style="color: green;"></i></td>
+      //   <td>
+      //     <button type="button" class="btn btn-primary"><i class="fa-solid fa-jet-fighter"></i></i></button>
+      //     <button type="button" class="btn btn-dark"><i class="fa-solid fa-ship"></i></button>
+      //     <button type="button" class="btn btn-info"><i class="fa-solid fa-car-burst"></i></button>
+      //   </td>
+      // </tr>`;
+      //FIN DE REGISTROS REPETIDOS
+
+      // TENER LOS 10 REGISTROS ORIGINALES
       content +=`
       <tr>
         <td>${index + 1}</td>
@@ -60,6 +79,7 @@ const listUsers = async () => {
           <button type="button" class="btn btn-info"><i class="fa-solid fa-car-burst"></i></button>
         </td>
       </tr>`;
+      //FIN DE LOS 10 REGISTROS ORIGINALES
     });
     tableBody_users.innerHTML = content;
   }catch(ex){
